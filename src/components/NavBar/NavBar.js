5f0894edc03a09cc90cd6221dffa80892582c777
RafaTrucco <rafatrucco2001@gmail.com>
2023-04-12 21:02:23 -0300

    cambios de class a classname

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,26 +5,26 @@ const NavBar = () => {
     return (
         <nav>
                        <section>
-                                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#"><img src={shop} alt="cart-widget" width="80" height="84"/></a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                                    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#"><img src={shop} alt="cart-widget" width="80" height="84"/></a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                             </button>
                         
-                            <div class="collapse navbar-collapse" id="navbarText">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#"> <button>Prenda Superior</button></a>
+                            <div className="collapse navbar-collapse" id="navbarText">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#"> <button>Prenda Superior</button></a>
                                 </li>
-                                <li class="nav-item">
-                                <a class="nav-link active" href="#"><button>Prenda Inferior</button></a>
+                                <li className="nav-item">
+                                <a className="nav-link active" href="#"><button>Prenda Inferior</button></a>
                                 </li>
-                                <li class="nav-item">
-                                <a class="nav-link active" href="#"><button>Calzado</button></a>
+                                <li className="nav-item">
+                                <a className="nav-link active" href="#"><button>Calzado</button></a>
                                 </li>
                             </ul>
-                            <span class="navbar-text">
+                            <span className="navbar-text">
                                 <CartWidget/>
                             </span>
                             </div>
